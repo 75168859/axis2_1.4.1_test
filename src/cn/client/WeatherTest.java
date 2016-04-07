275c041f1aeaed3316761efb507ecdf8652ae223
75168859@qq.com <75168859@qq.com>
2016-04-07 22:32:17 +0800

    axis2 1.4.1

---
+++ src/cn/client/WeatherTest.java
@@ -0,0 +1,27 @@
+package cn.client;
+
+import cn.ws.weather.WeatherWebServiceStub;
+import cn.ws.weather.WeatherWebServiceStub.ArrayOfString;
+import cn.ws.weather.WeatherWebServiceStub.GetWeatherbyCityName;
+import cn.ws.weather.WeatherWebServiceStub.GetWeatherbyCityNameResponse;
+import edu.emory.mathcs.backport.java.util.Arrays;
+
+/**
+ *天气预报 
+ */
+public class WeatherTest {
+
+	public static void main(String[] args) {
+		try {
+			WeatherWebServiceStub serviceStub = new WeatherWebServiceStub();
+			GetWeatherbyCityName getWeatherbyCityName4 = new GetWeatherbyCityName();
+			getWeatherbyCityName4.setTheCityName("北京");
+			GetWeatherbyCityNameResponse cityNameResponse = serviceStub.getWeatherbyCityName(getWeatherbyCityName4);
+			ArrayOfString arrayOfString = cityNameResponse.getGetWeatherbyCityNameResult();
+			System.out.println(Arrays.toString(arrayOfString.getString()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
